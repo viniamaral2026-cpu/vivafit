e96e87f669a5980af661698000ee097fa70939c9
vini amaral <viniamaral2026@gmail.com>
2026-01-03 10:28:22 +0000

    deve ficar como dados tecnico doestudo diz

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,8 +17,8 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
+        body: ['Roboto', 'sans-serif'],
+        headline: ['Poppins', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
