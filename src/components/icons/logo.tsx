c01e7d1f0bc048c81a4c1fe9f872c9031b06971f
vini amaral <viniamaral2026@gmail.com>
2026-01-03 14:24:23 +0000

    Image with src "https://scontent.fpoa13-1.fna.fbcdn.net/v/t39.30808-6/61

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -7,9 +7,9 @@ export function Logo(props: React.ComponentProps<"div">) {
       <Image
         src="https://scontent.fpoa13-1.fna.fbcdn.net/v/t39.30808-6/610776226_122094250413204574_3447186860395560382_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=uAPIqEeV8rEQ7kNvwErGvdr&_nc_oc=AdmojXGFJMImfmkx-SyecWayH4RjotRlN2U24mx_OQLTNTHhL2x-t96GaeaUQEqIi7ptAqkPbyRbeUPajpDIlWkU&_nc_zt=23&_nc_ht=scontent.fpoa13-1.fna&_nc_gid=PCg1f1GLhWjb7qWO_hgemA&oh=00_AfqCpYQG4SeSckTh3mcE9f4PeHYYKV_mZ5YKBMlrTCu6Ag&oe=695ED3E7"
         alt="VivaFit Logo"
-        width={100}
-        height={40}
-        className="object-contain"
+        width={250}
+        height={100}
+        className="h-auto w-full object-contain"
         unoptimized
       />
     </div>
