e81b726d7e9acc3947163a94f4fa763fd5ec53ab
vini amaral <viniamaral2026@gmail.com>
2026-01-03 18:22:47 +0000

    essa ta sem fundohttps://scontent.fpoa13-1.fna.fbcdn.net/v/t39.30808-6/6

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,7 +5,7 @@ export function Logo(props: React.ComponentProps<"div">) {
   return (
     <div {...props}>
       <Image
-        src="https://scontent.fpoa13-1.fna.fbcdn.net/v/t39.30808-6/611147366_122094480705204574_1697207361115252820_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=b8278c&_nc_ohc=YPFSw8Ff4SMQ7kNvwETveXy&_nc_oc=Adkrxe-9GvEtKmMrZ9ZNbeEjXf1Aor4Vv38XcF7ddbnAk8E-nh4Yqdgx2PFavNJUjuQmOjyQAVvp_nENs76B8CwA&_nc_zt=23&_nc_ht=scontent.fpoa13-1.fna&_nc_gid=bMIOpDWjQAXaRGn-9H1r3w&oh=00_AfrhgiuLN8CaBqMw4yxt7AJKHWTbgdbYBLCduZ7sCz6OJw&oe=695F18F3"
+        src="https://scontent.fpoa13-1.fna.fbcdn.net/v/t39.30808-6/610980894_122094488553204574_1173630654904122105_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=w_JTA7IAUPAQ7kNvwEBcTN-&_nc_oc=AdmNVNNY7yAC4_MNsCIBG6f5B7ia0Y0hwwVjPBroSSRUyPcJUY8Ji3EBXwuNV3UGf-RWKDv8-EWKETnRZ6i3wNxk&_nc_zt=23&_nc_ht=scontent.fpoa13-1.fna&_nc_gid=-K63nDAQaxDlndwipXQweA&oh=00_AfoDETGKyDd6kFfO8r5Q2Estc8Q6O353Yri78v1hkcFNog&oe=695F36BB"
         alt="VivaFit Logo"
         width={150}
         height={40}
