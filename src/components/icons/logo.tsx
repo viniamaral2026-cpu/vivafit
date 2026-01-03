b1b9e975d0c12f6279d6f37104b40a5893a190b5
vini amaral <viniamaral2026@gmail.com>
2026-01-03 18:10:59 +0000

    ajuste alogo do site para aceber no cabeçalho

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,10 +5,10 @@ export function Logo(props: React.ComponentProps<"div">) {
   return (
     <div {...props}>
       <Image
-        src="https://scontent.fpoa13-1.fna.fbcdn.net/v/t39.30808-6/610776226_122094250413204574_3447186860395560382_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_ohc=uAPIqEeV8rEQ7kNvwErGvdr&_nc_oc=AdmojXGFJMImfmkx-SyecWayH4RjotRlN2U24mx_OQLTNTHhL2x-t96GaeaUQEqIi7ptAqkPbyRbeUPajpDIlWkU&_nc_zt=23&_nc_ht=scontent.fpoa13-1.fna&_nc_gid=PCg1f1GLhWjb7qWO_hgemA&oh=00_AfqCpYQG4SeSckTh3mcE9f4PeHYYKV_mZ5YKBMlrTCu6Ag&oe=695ED3E7"
+        src="https://upload.wikimedia.org/wikipedia/commons/b/b5/Vivafit_logo.png"
         alt="VivaFit Logo"
-        width={250}
-        height={100}
+        width={150}
+        height={40}
         className="w-full h-auto object-contain"
         unoptimized
       />
